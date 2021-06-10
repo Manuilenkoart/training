040069f602d42b3b29c47355676cd40de21e6642
manuilenkoart <manuilenkoart@gmail.com>
2021-06-10 23:22:26 +0300

    локальные сервисы

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {AppCounterService} from './service/app-counter.service';
+import {LocalCounterService} from './service/local-counter.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  providers: [LocalCounterService]
 })
 export class AppComponent {
 
-  constructor(public appCounterService: AppCounterService) {
+  constructor(public appCounterService: AppCounterService,
+              public localCounterService: LocalCounterService) {
   }
 }
