2c58bddc9fb7ff7824cb052e6847db5e85f0e317
manuilenkoart <manuilenkoart@gmail.com>
2021-06-10 23:09:37 +0300

    создание сервиса

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {AppCounterService} from './service/app-counter.service';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [AppCounterService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
