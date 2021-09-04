21978dde710457b6703424211c2db3d8f9e64722
manuilenkoart <manuilenkoart@gmail.com>
2021-09-04 18:31:00 +0300

    init component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,11 +6,5 @@ import {Component} from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  img = 'https://hsto.org/files/230/81c/56d/23081c56d81744a686c0916ba25a2e2b.png';
 
-  constructor() {
-    setTimeout(() => {
-      this.img = 'https://cdn.iconscout.com/icon/free/png-256/angular-226066.png';
-    }, 4000);
-  }
 }
