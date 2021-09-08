33dacc4e24939519172011b1fc0553425fd30e60
manuilenkoart <manuilenkoart@gmail.com>
2021-09-08 22:18:10 +0300

    добавление событий

--- src/app/directives/style.directive.ts
+++ src/app/directives/style.directive.ts
@@ -1,4 +1,4 @@
-import {Directive, ElementRef, Renderer2} from '@angular/core';
+import {Directive, ElementRef, HostListener, Renderer2} from '@angular/core';
 
 @Directive({
   selector: '[appStyle]'
@@ -6,6 +6,16 @@ import {Directive, ElementRef, Renderer2} from '@angular/core';
 export class StyleDirective {
   constructor(private elRef: ElementRef,
               private renderer: Renderer2) {
+  }
+
+  @HostListener('click', ['$event.target']) onClick(event: Event): void {
+    console.log(event);
+  }
+
+  @HostListener('mouseenter') onEnter(): void {
     this.renderer.setStyle(this.elRef.nativeElement, 'color', 'blue');
+  }
+  @HostListener('mouseleave') onLeave(): void {
+    this.renderer.setStyle(this.elRef.nativeElement, 'color', null);
   }
 }
