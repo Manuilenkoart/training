58d7d8aeba747d115595e8ccad0ebacdde2ce61a
manuilenkoart <manuilenkoart@gmail.com>
2021-09-11 17:08:21 +0300

    фильтрация списков

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,21 @@
 import { Component } from '@angular/core';
 
+export interface Post {
+  title: string
+  text: string
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  date: Date = new Date()
-  float: number = 0.42
-  obj = {
-    a: 1,
-    b: {
-      c: 2,
-      d: {
-        e: 3,
-        f: 4
-      }
-    }
-  };
+ posts: Array<Post> = [
+   {title: 'Beer', text: 'The best beer in the world'},
+   {title: 'Bread', text: 'The best bread in the world'},
+   {title: 'JS', text: 'The best in the world'}
+ ];
+  search = '';
+  searchField = 'title'
 }
