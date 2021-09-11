412cab06641d4a4ab934bc0f179b763341d68dbf
manuilenkoart <manuilenkoart@gmail.com>
2021-09-11 16:08:58 +0300

    ещё пайпы

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   date: Date = new Date()
+  float: number = 0.42
+  obj = {
+    a: 1,
+    b: {
+      c: 2,
+      d: {
+        e: 3,
+        f: 4
+      }
+    }
+  };
 }
